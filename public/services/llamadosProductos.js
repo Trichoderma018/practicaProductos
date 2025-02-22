9d27dc4afe61425e044757e04536fb29c44655e0
juan <juan.paq72@gmail.com>
2025-02-21 21:34:51 -0600

    Modifique el endpoint de el get

--- public/services/llamadosProductos.js
+++ public/services/llamadosProductos.js
@@ -1,6 +1,6 @@
 async function getUsers() {
     try {
-        const response = await fetch('https://api.hacienda.go.cr/fe/ae?identificacion=207360415', {
+        const response = await fetch("http://localhost:3000/products", {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json'
